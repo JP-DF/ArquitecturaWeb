5381795ab800cdbcb24e70a4e4d284974f58fcc8
JP-DF <31259242+JP-DF@users.noreply.github.com>
2017-09-07 10:25:59 -0300

    Update DB

--- node-api-rest/app.js
+++ node-api-rest/app.js
@@ -4,66 +4,33 @@ var express = require("express"),
     methodOverride = require("method-override");
     mongoose = require('mongoose');
 
+//llamo a la libreria MongoDB	
+var mongoose = require('mongoose');
 
-// -----------CONEXION BASE DE DATOS----------------
-	var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
-
-  // Conexion URL DB
-var url = 'mongodb://localhost:27017/dbproyecto';
-
-// Metodo de conexion al server DB
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Conectado correctamente al server MongoDB mongodb://localhost:27017/dbproyecto");
-
-  //db.close();
+//Permite conexion a la Base dbproyecto 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/dbproyecto', function(error){
+   if(error){
+      throw error; 
+   }else{
+      console.log('Conectado a MongoDB');
+   }
+    console.log('Conectado a MongoDB');
 });
-//----------FIN CONEXION BASE DE DATOS--------------
-
-// Middlewares
+	
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
-/*
-// Import Models and controllers
-var models     = require('./models/dbproyecto')(app, mongoose);
-var DBProyectoCtrl = require('./controllers/dbproyecto');
-*/
-
-// Ejemplo de ruta
 var router = express.Router();
 
-// -----------IMPLEMENTO UN GET----------------
 router.get('/', function(req, res) {
-   res.send("DESARROLLO DE API REST DFIALA JLOPREATO");
+   res.send("DESARROLLO DE API FIALA - LOPREATO");
 });
 
 app.use(router);
 
-/*
-// API rutas
-var dbproyecto = express.Router();
-
-
-dbproyectohows.route('/dbproyecto')
-  .get(TVShowCtrl.findAllDBProyecto)
-  .post(TVShowCtrl.addDBProyecto);
-
-dbproyecto.route('/dbproyecto/:id')
-  .get(DBProyectoCtrl.findById)
-  .put(DBProyectoCtrl.updateDBProyecto)
-  .delete(DBProyectoCtrl.deleteDBProyecto);
-
-app.use('/api', dbproyecto);
-
-*/
-
-
- 
-app.listen(3000, function() {
-  console.log("Node server running on http://localhost:3000");
+  app.listen(3000, function() {
+    console.log("Node OK conectado a http://localhost:3000");
+  
 });
-
-
