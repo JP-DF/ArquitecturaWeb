716e743d9ceb9b5f2048c18b252f8166949ef46c
JP-DF <31259242+JP-DF@users.noreply.github.com>
2017-09-06 23:37:25 -0300

    Actualizacion

--- node-api-rest/app.js
+++ node-api-rest/app.js
@@ -4,6 +4,20 @@ var express = require("express"),
     methodOverride = require("method-override");
     mongoose = require('mongoose');
 
+	var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+  // Connection URL
+var url = 'mongodb://localhost:27017/dbproyecto';
+
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+
+  db.close();
+});
+	
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
@@ -18,20 +32,18 @@ router.get('/', function(req, res) {
 app.use(router);
 
 // -----------CONEXION BASE DE DATOS----------------
+
 /*
 mongoose.connect('mongodb://localhost/dbproyecto', function(err, res) {
   if(err) {
     console.log('ERROR: connectando a la base de datos. ' + err);
    
   }
- */
+*/
  
 app.listen(3000, function() {
   console.log("Node server running on http://localhost:3000");
 });
-
-
-
 
 
 
